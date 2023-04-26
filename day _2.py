024bf3ca20b10de751c3fc4a7049bbe65af8a64f
Claudiu Petre <claudiu.gpetre@gmail.com>
2023-04-26 06:28:42 +0100

    other

--- day _2.py
+++ day _2.py
@@ -34,3 +34,52 @@ first_digit = int(number[0])
 second_digit = int(number[1])
 result = first_digit + second_digit
 print(result)
+
+# Mathematical operations
+3 + 5
+7 - 6
+2 * 4
+print(6 / 2) // float result
+2 ** 2 (3, ..) // exponential (to the power of 2, 3, etc)
+
+    # PEMDAS LR: 
+    # ()parentheses, 
+    # ** exponential, 
+    # * multiplication, 
+    # / division, 
+    # + addition, 
+    # - subtraction
+    # LR left to right
+print(3 * 3 + 3 / 3 - 3) // 7
+# Change the above to obtain 3.0
+print(3 * (3 + 3) / 3 - 3) // 3
+
+# Challenge: calculate BMI = weight / 2*height
+height = input('enter your height in m: ')
+weight = input('enter your weight in kg: ')
+
+bmi = weight / float(height) **2
+bmi_integer =  int(bmi)
+print(bmi_integer)
+
+# Rounding
+print(round(8 / 3)) // 2.6666 = 3
+print(round(8 / 3, 2)) // 2.6666 = 2.67 (2 decimal points)
+
+print(8 // 3) // flow division gives you an ROUNDED INTEGER
+
+# keeping and updating scores..
+score = 0
+score += 1
+score -= 1
+score *= 1
+score /= 1
+
+print(score) // 1
+
+#  F strings
+score = 0
+height = 1.75
+isSwimming = True
+
+print(f'your score is {score}, your height is {height} and you can swim {isSwimming}')
