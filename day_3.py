4dc8dc19532fa3d0c25c893afd669076db1a040d
Claudiu Petre <claudiu.gpetre@gmail.com>
2023-04-29 18:28:31 +0100

    almost done

--- day_3.py
+++ day_3.py
@@ -15,11 +15,11 @@ if height >= 120:
     print('You can ride')
     age = int(input('What is your age? '))
     if age < 12:
-        print('Please pay $5')
+        print('Child tickets are $5')
     elif (age <= 18):
-        print('Please, pay 7$')
+        print('Youth tickets are 7$')
     else:
-        print('Please pay $12')
+        print('Adult tickets are $12')
 else:
     print('Sorry, grow up!')
 
@@ -39,3 +39,99 @@ elif bmi < 35:
     print(f'Your bmi is {bmi}, you are obese')
 else:
     print(f'Your bmi is {bmi}, you arr clinically obese')
+
+# Challenge: This is how you work out whether if a particular year is a leap year.
+# on every year that is evenly divisible by 4 
+# **except** every year that is evenly divisible by 100 
+# **unless** the year is also evenly divisible by 400
+year = int(input("Which year do you want to check? "))
+
+if year % 4 == 0:
+    print('Leap year.')
+elif year % 100 == 1:
+    print('Leap year')
+elif year % 400 == 0:
+    print('Leap year')
+else:
+    print('Not leap year.')
+
+
+# Multiple IFs in succession
+height = int(input('What is your height in cm? '))
+bill = 0 
+if height >= 120:
+    print('You can ride')
+
+    age = int(input('What is your age? '))
+    if age < 12:
+        bill = 5
+        print('Child tickets are $5')
+    elif (age <= 18):
+        bill = 7
+        print('Youth tickets are 7$')
+
+    elif age >= 45 and age<= 55:
+        print('Have a free ride on us') // AND operator
+
+    else:
+        bill = 12
+        print('Adult tickets are $12')
+
+    wants_photo = input('Do you want a photo? Y or N')
+    if wants_photo == 'Y':
+        bill += 3
+    print(f'Your final bill is ${bill}')
+
+else:
+    print('Sorry, grow up!')
+
+# #  Challenge: Pizza Delivery
+print("Welcome to Python Pizza Deliveries!")
+size = input("What size pizza do you want? S, M, or L ")
+add_pepperoni = input("Do you want pepperoni? Y or N ")
+extra_cheese = input("Do you want extra cheese? Y or N ")
+
+bill = 0
+if size == 'S':
+  bill += 15
+elif size == 'M':
+  bill += 20
+else:
+  bill += 25
+
+if add_pepperoni == 'Y':
+  if size == 'S':
+    bill +=2
+  else:
+    bill += 3
+
+if extra_cheese == 'Y':
+  bill += 1
+
+print(f'Your final bill is: ${bill}')
+
+# OR
+bill = 0
+
+if size == 'S':
+    bill += 15
+    if add_pepperoni == 'Y':
+        bill += 2
+    if extra_cheese == 'Y':
+        bill += 1
+
+if size == 'M':
+    bill += 20
+    if add_pepperoni == 'Y':
+        bill += 3
+    if extra_cheese == 'Y':
+        bill += 1
+
+if size == 'L':
+    bill += 25
+    if add_pepperoni == 'Y':
+        bill += 3
+    if extra_cheese == 'Y':
+        bill += 1
+
+print(f'Your final bill is: ${bill}')
