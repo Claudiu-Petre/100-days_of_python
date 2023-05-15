c94f1431eb2027065c7437f6a8089d3551b794c7
Claudiu Petre <claudiu.gpetre@gmail.com>
2023-05-15 07:00:04 +0100

    initial

---
+++ day_6.py
@@ -0,0 +1,13 @@
+# Functions
+    # create your own function:
+def my_function():
+    print('Hello')
+    print('Bye')
+my_function()
+
+# Code Blocks
+# While loop 
+# Project: Karel the robot
+
+
+
