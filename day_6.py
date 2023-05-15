831ff3a8f227a932ee2836bd3e9b7b6b34f0a2b7
Claudiu Petre <claudiu.gpetre@gmail.com>
2023-05-15 07:59:19 +0100

    more

--- day_6.py
+++ day_6.py
@@ -5,9 +5,30 @@ def my_function():
     print('Bye')
 my_function()
 
+#  Challenge: Reeborg's World
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+    
+def jump():
+    move()
+    turn_left()
+    move()
+    turn_right()
+    move()
+    turn_right()
+    move()
+    turn_left()
+
+for step in range(6):
+    jump()
+
+
 # Code Blocks
 # While loop 
 # Project: Karel the robot
+
 
 
 
